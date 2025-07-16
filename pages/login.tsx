dc9a68880a29603e31d58e3976483a5f6b7b5d40
Mexos <mexospro@gmail.com>
2025-07-16 21:37:04 +0200

    test

--- pages/login.tsx
+++ pages/login.tsx
@@ -21,7 +21,7 @@ export default function Login() {
           className="flex px-12 py-2 text-lg tracking-widest uppercase rounded-full focus:outline-none bg-primary hover:bg-opacity-80"
           onClick={handleLogin}
         >
-          Login
+          Login !
         </button>
       </div>
     </Layout>
